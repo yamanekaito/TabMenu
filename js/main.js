d6c58d1db16e5aa7f20cb6fbbda194dc0638ec40
yamanekaito <yamanekaito0702@gmail.com>
2020-06-07 07:49:36 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 'use strict';
 {
   const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
   //配列
-  menuItems.forEach(item => {
-    item.addEventListener('click' ,e => {
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click' ,e => {
       e.preventDefault();
       //a要素はクリックすると他のページに飛ぶがそれをキャンセルする
-      item.classList.add('active');
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+      //menuItemsのactiveクラスをすべて外す
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
     })
   });
 }
