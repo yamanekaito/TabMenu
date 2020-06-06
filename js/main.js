5e06345d79b462b88f859b421bf429a69d3aaae9
yamanekaito <yamanekaito0702@gmail.com>
2020-06-06 17:32:28 +0900

    lesson7

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+  //配列
+  menuItems.forEach(item => {
+    item.addEventListener('click' ,e => {
+      e.preventDefault();
+      //a要素はクリックすると他のページに飛ぶがそれをキャンセルする
+      item.classList.add('active');
+    })
+  });
+}
